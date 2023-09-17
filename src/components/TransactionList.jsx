dfab2d799cefd6f9c54de1f8601bdd435826619d
ruben-cxtx <juarez.ruben2606@gmail.com>
2023-09-17 09:48:44 -0500

    finished project

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -2,16 +2,15 @@ import React, { useContext } from 'react';
 import { GlobalContext } from '../components/context/GlobalState';
 import Transaction from './Transaction';
 
-const TransactionList = () => {
-  const {transactions}= useContext(GlobalContext);
+export const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
+
   return (
     <>
-    <h3>History</h3>
-    {transactions.map(transaction => (
-      <Transaction key={transaction.id} transaction={transaction} />
-    ))}
-    <ul id='list' className='list'>
-    </ul>
+      <h3>History</h3>
+      <ul className="list">
+        {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
+      </ul>
     </>
   )
 }
--- src/components/context/GlobalState.jsx
+++ src/components/context/GlobalState.jsx
@@ -1,22 +1,38 @@
-import React, { createContext, useReducer } from "react";
-import AppReducer from "./AppReducer";
+import React, { createContext, useReducer } from 'react';
+import AppReducer from './AppReducer';
 
+// Initial state
 const initialState = {
-    transaction: [
-        {text: 'food'}
-    ]
+  transactions: []
 }
 
-//create context
+// Create context
 export const GlobalContext = createContext(initialState);
 
-//provide component
-export const GlobalProvider = ({children}) => {
-    const [state, dispatch] = useReducer(AppReducer, initialState);
+// Provider component
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
 
-    return (
-        <GlobalContext.Provider value={{ transactions: state.transaction }}>
-            {children}
-        </GlobalContext.Provider>
-    )
+  // Actions
+  function deleteTransaction(id) {
+    dispatch({
+      type: 'DELETE_TRANSACTION',
+      payload: id
+    });
+  }
+
+  function addTransaction(transaction) {
+    dispatch({
+      type: 'ADD_TRANSACTION',
+      payload: transaction
+    });
+  }
+
+  return (<GlobalContext.Provider value={{
+    transactions: state.transactions,
+    deleteTransaction,
+    addTransaction
+  }}>
+    {children}
+  </GlobalContext.Provider>);
 }
